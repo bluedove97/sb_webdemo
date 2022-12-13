2d94202a6650b08462eefeb17ba9de54da76146d
bluedove <bluedove@DESKTOP-LM7IPTG>
2022-12-13 14:45:45 +0900

    init push

---
+++ webdemo/src/main/java/com/example/demo/WebdemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdemoApplication implements InitializingBean{
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdemoApplication.class, args);
+	}
+
+	@PostConstruct
+	public void test22() {
+		System.out.println("어느시점인지 보자 @PostConstruct");
+	}
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		
+		System.out.println("어느시점인지 보자 @afterPropertiesSet");
+
+	}
+
+}
